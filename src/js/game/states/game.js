83e8578ce229a5451409cd323b4230efd4746842
Anthony Finch <actionspeakslouder@gmail.com>
2016-12-09 19:20:51 -0800

    Adds PBW loading screen

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,8 +1,35 @@
 var game = {};
 
 game.create = function () {
-  var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-  logo.anchor.setTo(0.5, 0.5);
+    this.game.stage.backgroundColor = '#fce08c';
+    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY-50, 'logo');
+    this.logo.smoothed = false;
+    this.logo.scale.setTo(6, 6);
+    this.logo.anchor.setTo(0.5, 0.5);
+    this.logo.alpha = 0;
+
+    this.game.add.tween(this.logo).to(
+        {alpha: 1},
+        2000,
+        Phaser.Easing.Linear.None,
+        true,
+        0,
+        1000,
+        true
+    );
+
+    this.text = this.game.add.text(
+        this.game.world.centerX,
+        this.game.world.centerY + 100,
+        'loading...',
+        {fill: '#000'}
+    );
+    this.text.anchor.setTo(0.5, 0.5);
+
 };
+
+game.update = function () {
+};
+
 
 module.exports = game;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -1,7 +1,7 @@
 var preloader = {};
 
 preloader.preload = function () {
-  this.game.load.image('logo', 'images/phaser.png');
+  this.game.load.image('logo', 'images/wiz-face.png');
 };
 
 preloader.create = function () {
